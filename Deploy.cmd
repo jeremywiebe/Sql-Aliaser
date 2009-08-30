19d8f30304354e788aa74ade6b3f61f09a38d11e
jeremywiebe <jeremy.wiebe@gmail.com>
2009-08-29 19:40:51 -0500

    Adding Growl dll's to my local deployment batch file

--- Deploy.cmd
+++ Deploy.cmd
@@ -1,3 +1,4 @@
 xcopy /v /R /Y D:\Projects\Personal\SqlAliaser\src\SqlAliaser\bin\debug\SqlAliaser.exe D:\Utilities\SqlAliaser\
 xcopy /v /R /Y D:\Projects\Personal\SqlAliaser\src\SqlAliaser\bin\debug\SqlAliaser.exe.config D:\Utilities\SqlAliaser\
 
+xcopy /v /R /Y D:\Projects\Personal\SqlAliaser\src\SqlAliaser\bin\debug\Growl*.dll D:\Utilities\SqlAliaser\
